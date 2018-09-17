17f34d1611ad2b89fb7adec8496d45fcfe5e39a1
lokeshkumaraguru <lkumaraguru@mail.csuchico.edu>
2018-09-17 16:21:06 -0700

    Completed with boolean expressions and control flow statements.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Author:
+//Author: Lokesh Kumaraguru
 #include <iostream>
 
 using namespace std;
@@ -10,15 +10,22 @@ int main()
   cout<<"Guess a number between 1-10: ";
   cin>>guess;
 
-
-  //when they guess correctly (7), display:
-  cout<<"YES! Lucky number 7!\n";
-
-  //when they guess just off by one (either 6 or 8), display:
-  cout<<"Oh, so close!\n";
-
-  //when they guess any other number, display:
-  cout<<"Waaay off, sorry.\n";
+  if(guess == 7)
+  {
+    cout<<"YES! Lucky number 7!\n";
+  }
+  else if(guess == 6)
+  {
+    cout<<"Oh, so close!\n";
+  }
+  else if(guess == 8)
+  {
+    cout<<"Oh, so close!\n";
+  }
+  else
+  {
+    cout<<"Waaay off, sorry.\n";
+  }
 
   return 0;
 }
